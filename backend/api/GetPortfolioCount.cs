a0bfb5d8fc731771882dbc0ce41ded7449bd9d4c
Charles Cunningham Jr <104052242+chrlsC08@users.noreply.github.com>
2023-05-13 17:34:33 -0400

    Updates

---
+++ backend/api/GetPortfolioCount.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace Company.Function
+{
+    public static class GetPortfolioCount
+    {
+        [FunctionName("GetPortfolioCount")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName:"AzurePortfolio", collectionName: "Counter", ConnectionStringSetting = "AzurePortfolioConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+            [CosmosDB(databaseName:"AzurePortfolio", collectionName: "Counter", ConnectionStringSetting = "AzurePortfolioConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
